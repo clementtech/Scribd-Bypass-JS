be6cbff14833fd87ff5792061d63aaa3ca851109
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-10-09 15:50:58 +0800

    some small progress but not working yet

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("This tools is for educational purposes ONLY! \nThe author is not responsible for any misused or legal consequences.");
+// alert("This tools is for educational purposes ONLY! \nThe author is not responsible for any misused or legal consequences.");
+
+let scribd_original_link = document.getElementById('scribd_original_link');
+
+
+
